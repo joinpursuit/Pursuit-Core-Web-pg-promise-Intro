2157a85a49bb71e9ca5d3e2343d8cc7a15ed278d
your name here <your_email_here>
2020-01-25 15:38:29 -0500

    finished new post

--- routes/posts.js
+++ routes/posts.js
@@ -24,7 +24,7 @@ router.get('/all', async(req, res, next) => {
   // res.send('Sending all posts!');
 });
 
-router.get('/:user_id', async(req, res) => {
+router.get('/:user_id', async(req, res, next) => {
   try {
     let postsByUser =await db.any("SELECT * FROM posts WHERE poster_id = $1",[req.params.user_id])
     res.status(200).json({
@@ -38,8 +38,18 @@ router.get('/:user_id', async(req, res) => {
   // res.send('Sending all post from this user!');
 });
 
-router.post('/register', (req, res) => {
-  res.send('Creating new post!');
+router.post('/register', async(req, res, next) => {
+  // res.send('Creating new post!');
+  try {
+    let post =await db.one("INSERT INTO posts (poster_id, body) VALUES($1,$2) RETURNING *", [req.body.poster_id,req.body.bodyText])
+    res.status(200).json({
+        post,
+        status:"success",
+        message:"create the new post"
+    })
+} catch (error) {
+    next(error)
+}
 });
 
 module.exports = router;
